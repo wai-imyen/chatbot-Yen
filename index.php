4d191c1af9f9277473be17cf60e82f1e023d7b16
wai_imyen <wai.imyen@gmail.com>
2018-07-04 18:37:53 +0800

    修改界接參數

--- index.php
+++ index.php
@@ -6,33 +6,40 @@ $db = new DB($DB_USER,$DB_PASS,$DB_NAME,$DB_HOST);
 
 $m_id = 172;
 
-// 與FB及Wit.ai 界接的參數
-$hubVerifyToken = 'chatbotYen486'; // 這裏寫的字串，後面我們設定 webhooks 時要一致
+// 與FB及Wit.ai 界接參數
+$hubVerifyToken = 'chatbotYen486'; // 與設定 webhooks 一致
 $accessToken = 'EAACBMI5Xng0BAEiH8vx2gwP9biqKQNeOk5qwW2JrN9bAAcNTDLanQHPoUqUeUTfNsuOfFDy6KOzVE4hcHCSLrhQYHgqRj4QH8mMYMmtYdRXs3GbiWcZAsvZCZAT4QybSADc1LZCZAa89kQ1DRRYagcuaiSYlQQ6jAoYvNK1vnUOcAB3v16TQX';
 
 
-// check token at setup
-if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe') {
-    if($_REQUEST['hub_verify_token'] == $hubVerifyToken) {
+// 檢查 token 設定
+if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe') 
+{
+    if($_REQUEST['hub_verify_token'] == $hubVerifyToken) 
+    {
         echo $_REQUEST['hub_challenge'];
         exit();
     }
 }
 
-
-
 // 接收來自 Facebook 的 input
 $input = json_decode(file_get_contents('php://input'), true);
 
+// 傳送者 id
 $send_id = $input['entry'][0]['messaging'][0]['sender']['id'];
 
+// 訊息陣列
 $msg_array = $input['entry'][0]['messaging'][0];
 
+// 接收訊息
 $msg = isset($input['entry'][0]['messaging'][0]['message']['text']) ? $input['entry'][0]['messaging'][0]['message']['text']: '' ;
+
+// 訊息 postback
 $messaging_postback = isset($input['entry'][0]['messaging'][0]['postback']['payload']) ? $input['entry'][0]['messaging'][0]['postback']['payload']: '' ;
 
+// 快速回覆
 $quick_replies_payload = $input['entry'][0]['messaging'][0]['message']['quick_reply']['payload'];
 
+// 傳送者資料
 $profile = get_sender_profile($send_id,$accessToken);
 
 $reply = '';
@@ -55,7 +62,6 @@ $pj_array = array();
 
 foreach ($result as $data)
 {
-    
 	array_push($pj_array, $data['pj_name']);
 }
 
@@ -63,14 +69,13 @@ foreach ($result as $data)
 
 if ($messaging_postback || $msg) {
 
-	if ($messaging_postback == 'first_hand_shake') {
-
+	if ($messaging_postback == 'first_hand_shake') 
+	{
 		$reply = 'Hello ~ '.$profile['first_name'];
-
 	}
 
-	elseif($messaging_postback == 'search_rank' || $msg == '查詢排名') {
-
+	elseif($messaging_postback == 'search_rank' || $msg == '查詢排名') 
+	{
 		$reply = '輸入您要查詢的專案';
 
 		$quick_replies =  [];
@@ -82,22 +87,17 @@ if ($messaging_postback || $msg) {
 		        "title" => $data['pj_name'],
 		        "payload" => $data['pj_name']
 	    	];
-
 	    	array_push($quick_replies, $push_data);
 		}
+	}
+	elseif (in_array($msg, $pj_array))
+	{
 
-
-
-	}elseif (in_array($msg, $pj_array)) {
-
-
-	
 		$reply =	'"'.$msg.'" 關鍵字排名 :'.chr(10).chr(10);
-
-
 
 		/* 查詢專案資料 start */
 
+		// 取得專案資料
 		$columns = 'pj_id, update_time, pj_domain';
 
 		$table = 'project';
@@ -110,15 +110,15 @@ if ($messaging_postback || $msg) {
 
 		$project_result =  $db->select($columns,$table,$where,$others='',$data,$format,$debug=0)[0];
 
+		// 專案編號
 		$pj_id = $project_result['pj_id'];
 
+		// 專案網址
 		$pj_domain = $project_result['pj_domain'];
 
 		$update_date = substr($project_result['update_time'], 0,10);
 
-
-
-
+		// 查詢關鍵字資料
 		$columns = 'keyword';
 
 		$table = 'keyword';
@@ -133,7 +133,7 @@ if ($messaging_postback || $msg) {
 
 		foreach ($keyword_result as $keyword_data)
 		{
-		    
+		    // 查詢歷史紀錄
 			$columns = '*';
 
 			$table = 'history';
@@ -148,6 +148,7 @@ if ($messaging_postback || $msg) {
 
 			$history_result = $db->select($columns,$table,$where,$others,$data,$format,$debug=0)[0];
 
+			// 回覆訊息
 			$reply .=	$history_result['keyword'].' : '.$history_result['rank'].chr(10);
 		}
 
@@ -156,36 +157,26 @@ if ($messaging_postback || $msg) {
 
 		/* 查詢專案資料 end */
 
-	}elseif ($messaging_postback == 'search_case' || $msg == '查詢方案') {
-		
+	}
+	elseif ($messaging_postback == 'search_case' || $msg == '查詢方案') 
+	{
 		$reply = '立馬幫您查詢方案資料';
-
-
-	}elseif ($messaging_postback == 'ask_questions' || $msg == '詢問問題') {
-		
+	}
+	elseif ($messaging_postback == 'ask_questions' || $msg == '詢問問題') 
+	{
 		$reply = '問！';
-
-	}else{
-
+	}
+	else
+	{
 		$reply = '哇聽謀哩勒共蝦';
-
 	}
 	
-
 	send_text_message($send_id,$reply,$quick_replies,$accessToken);
 
 	die();
-
 }
 
-
-
-
-
-
-
-
-
+// 取得傳送者資料
 function get_sender_profile($send_id,$accessToken)
 {
 	$ch = curl_init();
@@ -200,6 +191,7 @@ function get_sender_profile($send_id,$accessToken)
 	return $profile;
 }
 
+// 發送訊息
 function send_text_message($send_id,$reply,$quick_replies,$accessToken){
 
 	$response = [
@@ -210,7 +202,6 @@ function send_text_message($send_id,$reply,$quick_replies,$accessToken){
 	    	"quick_replies" => $quick_replies
 
 	    ]	// -- message
-
 	];	// -- $response
 
 	$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
@@ -223,9 +214,4 @@ function send_text_message($send_id,$reply,$quick_replies,$accessToken){
 	exit();
 
 	return ;
-
-	
 }
-
-
-
